363ddf688b8063eb8912c534404cb47131a9799d
CJ-Patel <cjai.patel@gmail.com>
2024-09-29 18:02:34 +0200

    Add files via upload

---
+++ Task3/menu.py
@@ -0,0 +1,8 @@
+print("Please enter your 3 favourite food items on a menu:")
+item1 = input("Please enter item number 1: ")
+item2 = input("Please enter item number 2: ")
+item3 = input("Please enter item number 3: ")
+print("Order Confirmation! You have ordered:")
+print(item1)
+print(item2)
+print(item3)
