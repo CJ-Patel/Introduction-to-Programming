3ac3bdc507de44d713f66354fe1858fba264534d
CJ-Patel <cjai.patel@gmail.com>
2024-09-29 18:03:04 +0200

    Add files via upload

---
+++ Task4/details.py
@@ -0,0 +1,11 @@
+#CJ Patel
+#Task4 - details.py
+#------------------------------------------------------------#
+#request input from user
+name = input("Please enter your name: ")
+age = input("Please enter your age: ")
+house_number = input("Please enter your house number: ")
+street_name = input("Please enter your street name: ")
+
+#print output
+print("This is " + name + " he is " + age + " and lives at house number " + house_number + " on " + street_name + ".")
