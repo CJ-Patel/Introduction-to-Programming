abb259c571a4d1603fc49f21022cbddeeff2e539
CJ-Patel <cjai.patel@gmail.com>
2024-10-04 18:38:44 +0200

    Add files via upload

---
+++ Task7/baby.py
@@ -0,0 +1,11 @@
+#CJ Patel
+#Task7 - baby.py
+#------------------------------------------------------------#
+#request input from user
+DOY = int(input("Please enter the year that you were born in: "))
+
+#calculation of age and display appropriate message
+if(2020-DOY >= 18):
+	print("Congrats you are old enough")
+#else:
+#	print("You are too young")
