5dbbcfc9298519fe1aa369b376acbb67dff23efe
CJ-Patel <cjai.patel@gmail.com>
2024-10-16 23:31:29 +0200

    Add files via upload

---
+++ Task14/task1.py
@@ -0,0 +1,8 @@
+#CJ Patel
+#Task14 - task1.py
+#------------------------------------------------------------#
+#request input from user
+num = int(input("Please enter a number: "))
+
+for i in range (1, 13):
+	print(str(num) + " x " + str(i) + " =  " + str(num*(i)))
